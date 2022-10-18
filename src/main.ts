1f5e5c0909d257b32d57b224c091c004732c0100
Michael Wu <maico910@gmail.com>
2022-10-18 14:45:41 -0300

    refactor: plugin install

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import plugins from "./plugins"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// TODO: add type for plugin options in app.use
+app.use(plugins, {})
+
+app.mount('#app')
