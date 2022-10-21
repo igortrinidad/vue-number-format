8d9bf5ca7a8ce8b04f0f7d7ec77e0449dc8ae732
Michael Wu <maico910@gmail.com>
2022-10-20 23:51:15 -0300

    refactor: test to typescript

--- tests/unit/functions.spec.ts
+++ tests/unit/functions.spec.ts
@@ -1,3 +1,4 @@
+import { test, expect } from 'vitest';
 import { format, unformat } from '../../src/utils'
 
 test('it should format a given number to US$ currency format', () => {
--- tests/unit/vueNumberFormat.spec.ts
+++ tests/unit/vueNumberFormat.spec.ts
@@ -1,10 +1,15 @@
+import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import VueNumberFormat from '@/vue-number-format.vue'
+import VueNumberFormat from '@/components/VueNumberFormat.vue'
+import vueNumberFormatPlugin from '@/plugins'
 
 describe('vue-number-format.vue', () => {
   it('renders formatted currency value with the default options with passing any prop option', () => {
     const wrapper = mount(VueNumberFormat, {
-      propsData: { value: 123 }
+      props: { value: 123 },
+      global: {
+        plugins: [vueNumberFormatPlugin]
+      }
     })
     expect(wrapper.props().value).toBe(123)
     expect(wrapper.find('input').element.value).toBe('R$ 123,00')
@@ -12,7 +17,13 @@ describe('vue-number-format.vue', () => {
 
   it('renders formatted integer number 123 to US$ 1.23 on input', () => {
     const wrapper = mount(VueNumberFormat, {
-      propsData: { value: 123, options: { isInteger: true, prefix: 'US$ ', decimal: '.', thousand: ','} }
+      props: {
+        value: 123,
+        options: { isInteger: true, prefix: 'US$ ', decimal: '.', thousand: ','}
+      },
+      global: {
+        plugins: [vueNumberFormatPlugin]
+      }
     })
     expect(wrapper.props().value).toBe(123)
     expect(wrapper.find('input').element.value).toBe('US$ 1.23')
@@ -20,10 +31,12 @@ describe('vue-number-format.vue', () => {
 
   it('renders formatted integer number 123 to US$ 1.23 on input', () => {
     const wrapper = mount(VueNumberFormat, {
-      propsData: { value: 1.23, options: { prefix: '', suffix: 'kg', decimal: ',', thousand: '.', precision: 3} }
+      props: { value: 1.23, options: { prefix: '', suffix: 'kg', decimal: ',', thousand: '.', precision: 3} },
+      global: {
+        plugins: [vueNumberFormatPlugin]
+      }
     })
     expect(wrapper.props().value).toBe(1.23)
     expect(wrapper.find('input').element.value).toBe('1,230kg')
   })
-
 })
